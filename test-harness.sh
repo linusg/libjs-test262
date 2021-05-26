8b6a8e1b723a383cc8ad1bc86ac06ad5dd582870
Linus Groh <mail@linusgroh.de>
2021-05-26 01:19:54 +0100

    Check existence of built js in test-harness.sh
    We get a bunch of false positives otherwise.

--- test-harness.sh
+++ test-harness.sh
@@ -4,9 +4,16 @@ red=`tput setaf 9`
 green=`tput setaf 10`
 reset=`tput sgr0`
 
+js="serenity/Build/js"
+
+if [ ! -f "${js}" ]; then
+    echo "${js} not found, run setup.sh first"
+    exit 1
+fi
+
 for f in test262/harness/*.js; do
     filename=$(basename $f)
-    output=$(serenity/Build/js "${f}" 2>&1)
+    output=$("${js}" "${f}" 2>&1)
     if echo "${output}" | grep -q "SyntaxError"; then
         printf "[ ${red}FAIL${reset} ]"
     else
