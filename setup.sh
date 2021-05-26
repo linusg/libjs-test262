ca405f215df03c0bf4927580f203ca811de0eaff
Linus Groh <mail@linusgroh.de>
2021-05-26 23:02:17 +0100

    Add some logging to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env sh
 
+printf "Removing cloned serenity repo... "
 rm -rf serenity
+printf "done\n"
+
+printf "Removing cloned test262 repo... "
 rm -rf test262
+printf "done\n"
+
 git clone --depth 1 https://github.com/SerenityOS/serenity.git
 git clone --depth 1 https://github.com/tc39/test262.git
 
 mkdir -p serenity/Build
 cd serenity/Build
+
+printf "Running CMake...\n"
 cmake -GNinja -DBUILD_LAGOM=ON ../Meta/Lagom
+
+printf "Building Lagom js...\n"
 ninja js_lagom
