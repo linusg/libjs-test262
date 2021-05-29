714e34bb4073c667f09879e5036f952391ba9a59
Idan Horowitz <idan.horowitz@gmail.com>
2021-05-29 20:03:55 +0300

    Add test result emoji meanings to --help

--- main.py
+++ main.py
@@ -298,7 +298,8 @@ class Runner:
 
 def main() -> None:
     parser = ArgumentParser(
-        description="Run the test262 ECMAScript test suite with SerenityOS's LibJS"
+        description="Run the test262 ECMAScript test suite with SerenityOS's LibJS",
+        epilog=", ".join(f"{EMOJIS[result]} = {result.value}" for result in TestResult)
     )
     parser.add_argument(
         "-j",
