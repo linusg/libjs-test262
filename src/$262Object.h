24a7683863327b6509751493f6521b172e9c6ce9
Idan Horowitz <idan.horowitz@gmail.com>
2021-06-10 22:48:41 +0300

    Expose the $262.detachArrayBuffer harness helper

--- src/$262Object.h
+++ src/$262Object.h
@@ -24,5 +24,6 @@ private:
     AgentObject* m_agent { nullptr };
 
     JS_DECLARE_NATIVE_FUNCTION(create_realm);
+    JS_DECLARE_NATIVE_FUNCTION(detach_array_buffer);
     JS_DECLARE_NATIVE_FUNCTION(eval_script);
 };
